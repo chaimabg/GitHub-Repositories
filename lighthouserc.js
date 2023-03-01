0e0f63ed8a84c1c567308bc64318205fa559e8e3
Chaima Ben Ghanem <Chaima.BenGhanem@expensya.com>
2023-03-01 14:45:37 +0100

    add lighthouseci.js

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    ci: {
+        collect: {
+
+            url: "http://127.0.0.1:3000/",
+            numberOfRuns: 1, // Set low to speed up the test runs. Default is 3.
+            //headful: true, // Show the browser which is helpful when checking the config
+            settings: {
+                disableStorageReset: true, // Don't clear localStorage / IndexedDB / etc before loading the page
+                preset: 'desktop'
+            }
+        },
+        upload: {
+            target: 'lhci',
+           serverBaseUrl: 'http://localhost:9001',
+            token: '79a74b81-5b1c-4fd9-9ab2-1cfa367a09b5', // the build token provider by the wizard.
+            // admin token 1QCzd0ZPv0n8UnFyflZ1LqqDRxp6DIBolF7l4TaY
+        },
+    }
+};
